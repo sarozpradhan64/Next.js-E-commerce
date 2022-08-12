5b70ffe678a7a4ac56c4c2aed73ae13743b50a2e
sarozpradhan64 <sarozpradhan64@gmail.com>
2022-08-12 22:58:16 +0545

    form

---
+++ components/headerTitle.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Button, Text} from '@nextui-org/react'
+import { useRouter } from "next/router";
+
+
+export default function HeaderTitle({backBtn, title}) {
+    const router = useRouter();
+  return (
+    <>
+    { backBtn ? <Button
+    auto
+    onClick={() => router.back()}
+    css={{ borderRadius: "unset" }}
+    size={"sm"}
+    color="secondary"
+  >
+    Go Back
+  </Button> : ''}
+  <Text h2 css={{ mt: "1rem" }}>
+    {title}
+  </Text>
+  </>
+  )
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import Layout from '../components/layout'
+import { Grid, Button,Spacer, Textarea, Text, Input } from "@nextui-org/react";
+import React from "react";
+import HeaderTitle from "../components/headerTitle";
+import Layout from "../components/layout";
 
 export default function contact() {
   return (
     <Layout>
-    <div>contact</div>
+      <HeaderTitle title={"Contact Us"} backBtn={true} />
+      <Grid.Container gap={2}>
+        <Grid xs={12} sm={6} css={{display: 'flex', flexDirection: 'column', pt:'2rem'}}>
+
+          <form name='Nanu Contact Form' method="POST" netlify data-netlify="true" >
+          <input type="hidden" name="form-name" value="contact-form" />
+          <Input clearable underlined css={{width:'100%'}} labelPlaceholder="Full Name" />
+           <Spacer y={2} />
+          
+          <Input clearable underlined css={{width:'100%'}} labelPlaceholder="Email" type="email"/>
+           <Spacer y={2} />
+          
+          <Input clearable underlined css={{width:'100%'}} labelPlaceholder="Phone" type={'number'}/>
+           <Spacer y={2} />
+           <Textarea css={{width:'100%'}}
+          underlined
+          labelPlaceholder="Message"
+        />
+        <Spacer y={2} />
+        <Button color={'secondary'} css={{borderRadius:"unset"}} type='submit'>Send Message</Button>
+           </form>
+        </Grid>
+      </Grid.Container>
     </Layout>
-  )
+  );
 }
